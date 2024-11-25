2d481896f3a8b323967dc2570c6d8dc20f170a11
rzhb <rezahabibi96@gmail.com>
2024-11-25 13:28:18 +0900

    add model_train func

--- models/trainer.py
+++ models/trainer.py
@@ -101,3 +101,54 @@ def eval(model, device, dataloader, type=''):
     print(f'{type}, RMSE: {rmse}, MAE: {mae}')
 
     return rmse, mae, mape, y_pred, y_truth
+
+
+def model_train(train_dataloader, val_dataloader, config, device):
+    """
+    train the given model.
+
+    :param train_dataloader: data loader of training dataset.
+    :param val_dataloader: data loader of val dataset.
+    :param config: configuration to use.
+    :param device: device to use.
+
+    :return model: trained model.
+    """
+    model = ST_GAT(in_channels=config['N_HIST'], out_channels=config['N_PRED'],
+                   n_nodes=config['N_NODES'], dropout=config['P_DROPOUT'])
+    
+    optimizer = optim.Adam(model.parameters(), lr=config['P_LR'], 
+                           weight_decay=config['P_WEIGHT_DECAY'])
+    
+    loss_fn = torch.nn.MSELoss
+
+    model.to(device)
+
+    # train model for each epoch
+    for epoch in range(config['N_EPOCHS']):
+        loss = train(model, device, train_dataloader, optimizer, loss_fn, epoch)
+        print(f'loss: {loss:.3f}')
+
+        if epoch % 5 == 0:
+            train_rmse, train_mae, train_mape, _, _ = eval(model, device, train_dataloader, 'train')
+            val_rmse, val_mae, val_mape, _, _ = eval(model, device, val_dataloader, 'val')
+
+            writer.add_scalar(f"RMSE/train", train_rmse, epoch)
+            writer.add_scalar(f"MAE/train", train_mae, epoch)
+            writer.add_scalar(f"MAPE/train", train_mape, epoch)
+            
+            writer.add_scalar(f"RMSE/val", val_rmse, epoch)
+            writer.add_scalar(f"MAE/val", val_mae, epoch)
+            writer.add_scalar(f"MAPE/val", val_mape, epoch)
+
+    writer.flush()
+    timestr = time.strftime("%m-%d-%H%M%S")
+
+    torch.save({
+        "epoch": epoch,
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "loss": loss,
+    }, os.path.join(config['DIR_CHECKPOINT'], f'model_{timestr}'))
+
+    return model
