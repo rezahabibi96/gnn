77b6c987e79a56d19dba4e9371e2a17f6dd869a0
rzhb <rezahabibi96@gmail.com>
2024-11-27 21:58:58 +0900

    remove gat_mode dependency of ST_GAT model to hyperparam

--- dataloader/dataset_traffic.py
+++ dataloader/dataset_traffic.py
@@ -54,7 +54,7 @@ class TrafficDataset(InMemoryDataset):
 
         # calc W given D
         D = pd.read_csv(self.raw_file_names[0], header=None).values
-        W = from_dist_to_weight(D)
+        W = from_dist_to_weight(D, Config.PARAMS.HYPER['GAT_MODE'])
 
         # load and process dataset
         data = pd.read_csv(self.raw_file_names[1], header=None).values
