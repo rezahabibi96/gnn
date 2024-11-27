38533cdd07b4379a159becb77438485145453f99
rzhb <rezahabibi96@gmail.com>
2024-11-27 21:34:57 +0900

    decouple save ckpt fromtrainer.py

--- models/tools.py
+++ models/tools.py
@@ -1,22 +1,40 @@
+import os
+import time
 import torch
 
-from models.model_st_gat import ST_GAT
+from helpers import Config
+from helpers import Log
 
 
-def load_checkpoint(path_checkpoint, config):
+def save_checkpoint(epoch, model, optim_fn, loss):
+    """
+    save model given checkpoint.
+
+    :param epocj: epoch value.
+    :param model: trained model.
+    :param optim_fn: optim function.
+    :param loss: loss value. 
+    """
+
+    time_strf = time.strftime("%Y-%m-%d %H:%M:%S")
+
+    torch.save({
+        "epoch": epoch,
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optim_fn.state_dict(),
+        "loss": loss,
+    }, os.path.join(Config.PARAMS.DIR['CHECKPOINTS'], f'ckpt_{time_strf}'))
+
+def load_checkpoint(model, path):
     """
     load model from checkpoint.
 
-    :param path_checkpoint: path to checkpoint. 
-    :param config: configuration to use.
+    :param model: model structure.
+    :param path: checkpoint path. 
 
     :return model: loaded model.
     """
-
-    model = ST_GAT(in_channels=config['N_HIST'], out_channels=config['N_PRED'],
-                   n_nodes=config['N_NODES'])
-    
-    checkpoint = torch.load(path_checkpoint, map_location=torch.device('cpu'))
+    checkpoint = torch.load(path, map_location=torch.device('cpu'))
 
     model.load_state_dict(checkpoint['model_state_dict'])
 
