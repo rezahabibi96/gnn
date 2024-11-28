ec60b586d04721c12cc0eba011b7151493c731e9
rzhb <rezahabibi96@gmail.com>
2024-11-28 13:35:42 +0900

    grouping ACTIVE

--- helpers/ConfigHelper.py
+++ helpers/ConfigHelper.py
@@ -8,8 +8,7 @@ from helpers.LogHelper import Log
 
 
 class ConfigModel(BaseModel):
-    ACTIVE_ENVIRONTMENT: str = None
-    ACTIVE_DATA: str = None
+    ACTIVE: dict = {}
     CUDA: dict = {}
     DATA: dict = {}
     DIR: dict = {}
@@ -33,18 +32,17 @@ class Config:
         Log.info("load config from file {}".format(cls.__file_config__))
 
         cls.PARAMS = ConfigModel(
-            ACTIVE_ENVIRONTMENT=cls.__config_yaml__['ACTIVE_ENVIRONTMENT'],
-            ACTIVE_DATA=cls.__config_yaml__['ACTIVE_DATA'],
+            ACTIVE=cls.__config_yaml__['ACTIVE'],
             CUDA=cls.__config_yaml__['CUDA'],
             DATA=cls.__config_yaml__['DATA'],
             DIR=cls.__config_yaml__['DIR'],
-            HYPER=cls.__config_yaml__['MODEL']['HYPER'],
-            ST_GAT=cls.__config_yaml__['MODEL']['ST_GAT'],
+            HYPER=cls.__config_yaml__['HYPER'],
+            MODEL=cls.__config_yaml__['MODEL'],
         )
 
-        cls.PARAMS.DATA[cls.PARAMS.ACTIVE_DATA]['N_SLOTS'] = ( cls.PARAMS.DATA[cls.PARAMS.ACTIVE_DATA]['N_INTERVALS'] - 
-                                                              (cls.PARAMS.HYPER['N_HIST'] + cls.PARAMS.HYPER['N_PRED']) 
-                                                              + 1 )
+        cls.PARAMS.DATA[cls.PARAMS.ACTIVE['DATA']]['N_SLOTS'] = ( cls.PARAMS.DATA[cls.PARAMS.ACTIVE['DATA']]['N_INTERVALS'] - 
+                                                                  (cls.PARAMS.HYPER['N_HIST'] + cls.PARAMS.HYPER['N_PRED']) 
+                                                                  + 1 )
 
     @classmethod
     def parse_cli(cls):
@@ -55,15 +53,15 @@ class Config:
         
         active_group = parser.add_argument_group("ACTIVE", "active-related") 
         
-        active_group.add_argument('--ACTIVE_ENVIRONTMENT', '-ae', type=str, default=cls.PARAMS.ACTIVE_ENVIRONTMENT,
+        active_group.add_argument('--ACTIVE.ENVIRONTMENT', '-ae', type=str, default=cls.PARAMS.ACTIVE['ENVIRONTMENT'],
                                help="select environtment (e.g., development, production)")
-        active_group.add_argument('--ACTIVE_DATA', '-ad', type=str, default=cls.PARAMS.ACTIVE_DATA,
+        active_group.add_argument('--ACTIVE.DATA', '-ad', type=str, default=cls.PARAMS.ACTIVE['DATA'],
                                help="select data (e.g., PEMSD7, PEMSD8)")
 
-        cli_args = parser.parse_args()
-        for field, value in vars(cli_args).items():
+        active_args = active_group.parser.parse_args()
+        for field, value in vars(active_args['ACTIVE']).items():
             if value is not None:
-                setattr(cls.PARAMS, field, value)
+                cls.PARAMS.ACTIVE[field] = value
 
     @classmethod
     def load(cls, use_cli=True):
