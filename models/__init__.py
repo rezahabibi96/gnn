c6c103f1904ad200dfbcb7dbd8255d15ea9f26b7
rzhb <rezahabibi96@gmail.com>
2024-11-28 20:27:26 +0900

    rename from tools to ckpt.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
 from .model_st_gat import ST_GAT
 from .trainer import model_train, model_eval
-from .tools import save_checkpoint, load_checkpoint
+from .ckpt import save_checkpoint, load_checkpoint
