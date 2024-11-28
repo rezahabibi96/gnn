aa8933f5575240f236b3583da861d01008244fda
rzhb <rezahabibi96@gmail.com>
2024-11-28 16:30:25 +0900

    change dep from active data to arg data

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -9,15 +9,16 @@ def split_data(data, ratio):
     given data, split it into subsets of train, val, and test.
 
     :param data: data to split.
-    :param n_slots: possible number of sliding windows in a day.
-    :param ratio: (train, val, test) ratio.
+    :param ratio: (train, val, test) ratio specified by number of days.
 
     :return train, val, test: splitted data.
     """
+    name = data.name
+
     r_train, r_val, _ = ratio
 
-    n_train = Config.PARAMS.DATA[Config.PARAMS.ACTIVE['DATA']]['N_SLOTS'] * r_train
-    n_val = Config.PARAMS.DATA[Config.PARAMS.ACTIVE['DATA']]['N_SLOTS'] * r_val
+    n_train = Config.PARAMS.DATA[name]['N_SLOTS'] * r_train
+    n_val = Config.PARAMS.DATA[name]['N_SLOTS'] * r_val
 
     train = data[:n_train]
     val = data[n_train : n_train + n_val]
