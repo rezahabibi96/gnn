8326fb7cfb7b5c58747bf4e50e287d420bf6a17e
rzhb <rezahabibi96@gmail.com>
2024-12-02 15:18:20 +0900

    add func to convert many tfevent

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 from .math import norm_z, denorm_z, calc_mae, calc_rmse, calc_mape
 from .plot import plot_pred
-from .tf import from_tfevent_to_pandas
+from .tf import from_tfevent_to_pandas, from_many_tfevent_to_pandas
