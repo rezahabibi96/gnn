27e44503fb70835bf78c050da3b0b0f0074685fe
rzhb <rezahabibi96@gmail.com>
2024-12-02 13:03:36 +0900

    change name from dataloader.py to dataloader_sl.py for consistency

--- dataloader/__init__.py
+++ dataloader/__init__.py
@@ -1,3 +1,3 @@
 from .dataloader_cl import get_ctxs, get_joint
-from .dataloader import split_data, from_dist_to_weight
+from .dataloader_sl import split_data, from_dist_to_weight
 from .dataset_traffic import TrafficDataset
