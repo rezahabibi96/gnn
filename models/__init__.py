10993786e99594e15bd402ab3326f68ba3519650
rzhb <rezahabibi96@gmail.com>
2024-12-02 15:11:33 +0900

    move ckpt inside trainers/

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,1 @@
-from .ckpt import save_checkpoint, load_checkpoint
 from .model_st_gat import ST_GAT
--- trainers/__init__.py
+++ trainers/__init__.py
@@ -1,1 +1,2 @@
+from .ckpt import save_checkpoint, load_checkpoint
 from .trainer import train, eval, model_train, model_eval
