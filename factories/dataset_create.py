0231a031c22e52a5b480ed922b9a1cb8f77cd1e5
rzhb <rezahabibi96@gmail.com>
2024-12-02 15:41:24 +0900

    rename from create_dataset to dataset_create

---
+++ factories/dataset_create.py
@@ -0,0 +1,14 @@
+from helpers import Config
+from dataprocessors import TrafficDataset
+
+
+def create_dataset():
+    """
+    func to create dataset based on active data
+
+    :return dataset: created dataset.
+    """
+    if Config.PARAMS['ACTIVE']['DATA'] == 'PEMSD7':
+        dataset = TrafficDataset()
+
+    return dataset
