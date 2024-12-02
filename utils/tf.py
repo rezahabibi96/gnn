1d64979da0eaa8fff93f1a1baed2221b44fb6a61
rzhb <rezahabibi96@gmail.com>
2024-12-02 18:12:20 +0900

    add filename to df

--- utils/tf.py
+++ utils/tf.py
@@ -1,3 +1,4 @@
+import glob
 import traceback
 import pandas as pd
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
@@ -12,43 +13,33 @@ def from_tfevent_to_pandas(path):
 
     :return df: pandas df.
     """
-    data = pd.DataFrame({"event_metric": [], "value": [], "step": [], "wall_time": []})
-    
-    try:
-        event_accumulator = EventAccumulator(path, size_guidance={"scalars": 0})
-        event_accumulator.Reload()
+    data = pd.DataFrame({"metric": [], "value": [], "step": [], "name": [], "wall_time": []})
 
-        tags = event_accumulator.Tags()["scalars"]
-        for tag in tags:
-            event_list = event_accumulator.Scalars(tag)            
-            value = list(map(lambda x: x.value, event_list))
-            step = list(map(lambda x: x.step, event_list))
-            wall_time = list(map(lambda x: x.wall_time, event_list))
-            
-            runlog = {"event_metric": [tag] * len(step), "value": value, "step": step, "wall_time": wall_time}
-            runlog = pd.DataFrame(runlog)
-            
-            data = pd.concat([data, runlog])
-    
-    except Exception:
-        print("event file possibly corrupt: {}".format(path))
-        traceback.print_exc()
+    for file in glob.glob(path):
+        try:
+            event_accumulator = EventAccumulator(file, size_guidance={"scalars": 0})
+            event_accumulator.Reload()
+
+            tags = event_accumulator.Tags()["scalars"]
+            for tag in tags:
+                event_list = event_accumulator.Scalars(tag)            
+                value = list(map(lambda x: x.value, event_list))
+                step = list(map(lambda x: x.step, event_list))
+                wall_time = list(map(lambda x: x.wall_time, event_list))
+                
+                runlog = {"metric": [tag] * len(step), "value": value, "step": step, 
+                          "name": [file.split('.')[-1]] * len(step), "wall_time": wall_time}
+                runlog = pd.DataFrame(runlog)
+                
+                data = pd.concat([data, runlog])
+        
+        except Exception:
+            print("event file possibly corrupt: {}".format(file))
+            traceback.print_exc()
     
     return data
 
 
-def from_many_tfevent_to_pandas(path):
-    """
-    convert many tfevent log to pandas df.
-    https://stackoverflow.com/questions/71239557/export-tensorboard-with-pytorch-data-into-csv-with-python.
-
-    :param path: given path (tensorboard logs).
-
-    :return df: pandas df.
-    """
-    pass
-
-
 if __name__ == "__main__":
     # python3 -m utils.tf
-    from_tfevent_to_pandas('./artifacts/tensorboard').to_csv('csv.csv')
+    from_tfevent_to_pandas('./artifacts/tensorboard/2024-12-02 17:01:41/*').to_csv('csv.csv')
