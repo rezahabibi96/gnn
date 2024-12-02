eb8c058fb95ffebdf6929acfa77d18e93f267f46
rzhb <rezahabibi96@gmail.com>
2024-12-02 15:44:32 +0900

    rename from create_model to model__create

---
+++ factories/model_create.py
@@ -0,0 +1,16 @@
+from helpers import Config
+from models import ST_GAT
+
+
+def create_model():
+    """
+    func to create model based on active model
+
+    :return model: created model.
+    """
+    if Config.PARAMS['ACTIVE']['MODEL'] == 'ST_GAT':
+        model = ST_GAT(in_channels=Config.PARAMS.HYPER['N_HIST'], out_channels=Config.PARAMS.HYPER['N_PRED'],
+                       n_nodes=Config.PARAMS.DATA[Config.PARAMS.ACTIVE['DATA']]['N_NODES'], 
+                       dropout=Config.PARAMS.HYPER['DROPOUT'])
+        
+        return model
