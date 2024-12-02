e64c564fae330f0f41196cfb0bf96c4018ef5ba5
rzhb <rezahabibi96@gmail.com>
2024-12-02 13:50:54 +0900

    revised module to separate dataprocessors from dataloaders

---
+++ dataloaders/dataloader_sl.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from helpers import Config
+from helpers import Log
+
+
+def split_data(data, ratio):
+    """
+    given data, split it into subsets of train, val, and test.
+
+    :param data: data to split.
+    :param ratio: (train, val, test) ratio specified by number of days.
+
+    :return train, val, test: splitted data.
+    """
+    name = data.name
+
+    r_train, r_val, _ = ratio
+
+    n_train = Config.PARAMS.DATA[name]['N_SLOTS'] * r_train
+    n_val = Config.PARAMS.DATA[name]['N_SLOTS'] * r_val
+
+    train = data[:n_train]
+    val = data[n_train : n_train + n_val]
+    test = data[n_train + n_val:]
+
+    return train, val, test
